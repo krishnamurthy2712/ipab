ffca1ec4924cba7d437eab87a73386fbb3fca558
tech.deepu88 <tech.deepu88@gmail.com>
2012-06-07 18:16:28 +0000

    - Bribe analysis service integrated. - report services added with VOs. - need to refactor the code.

--- java-src/src/com/ipablive/core/IDidnotPaid.java
+++ java-src/src/com/ipablive/core/IDidnotPaid.java
@@ -4,8 +4,13 @@
 package com.ipablive.core;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import com.ipablive.datasource.ConnectionFactory;
+import com.ipablive.vo.DidNotPaidBribesVO;
+import com.ipablive.vo.PaidBribesVO;
 
 /**
  * @author 
@@ -49,5 +54,47 @@ public class IDidnotPaid
 	    return _didntBribeInstance;
 	  }
 	  
+	  public ArrayList<DidNotPaidBribesVO> viewDintPay(String criteria)
+	  {
+		  ArrayList<DidNotPaidBribesVO> didnotPayBribes = new ArrayList<DidNotPaidBribesVO>();
+		  String searchCriteria="";
+		  if(criteria.equals("All"))
+		  {
+			  searchCriteria = "";
+		  }
+		  
+		  String query = "SELECT bc.*, ct.city_name AS c_city, bd.dept_name, bt.trans_name FROM bd_dint_bribe bc, bd_dept bd, bd_transactions bt, bd_city ct WHERE bc.c_dept=bd.id AND bc.c_transaction=bt.id "+ searchCriteria+ "and bc.approved=1 and bc.c_city=ct.Id order by bc.id desc";
+		  
+		  try
+		  {
+			  Statement stmt = conn.createStatement();
+			  ResultSet rs = stmt.executeQuery(query);
+			  while(rs.next())
+			  {
+				  DidNotPaidBribesVO dpbv = new DidNotPaidBribesVO();
+				  dpbv.setId(rs.getInt(1));
+				  dpbv.setCName(rs.getString(2));
+				  dpbv.setCCity(rs.getString(3));
+				  dpbv.setCDept(rs.getString(4));
+				  dpbv.setCTransaction(rs.getString(5));
+				  dpbv.setOthersTransaction(rs.getString(6));
+				  dpbv.setCBribeResistedBy(rs.getString(7));
+				  dpbv.setCAdditionalInfo(rs.getString(8));
+				  //9
+				  dpbv.setOtherDept(rs.getString(10));
+				  //11,12,13
+				  dpbv.setCount(rs.getInt(14));
+				  dpbv.setCCity(rs.getString(15));
+				  dpbv.setDeptName(rs.getString(16));
+				  dpbv.setTransName(rs.getString(17));
+				  
+				  didnotPayBribes.add(dpbv);
+			  }
+		  }catch(Exception e)
+		  {
+			  
+		  }
+		  return didnotPayBribes;
+	  }
 	  
 }
