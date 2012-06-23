b60889ad3197ec094a275da2c1986b74ea06bdc2
krishnamurthy.svg@gmail.com <krishnamurthy.svg@gmail.com@6f921f18-4814-14e7-4169-a15d7f844ba5>
2012-06-23 20:00:22 +0000

    add comment module added. captcha updated to get numbers also.

---
+++ trunk/java-src/WebContent/comments/addComment.jsp
@@ -0,0 +1,48 @@
+<%
+String subject = request.getParameter("subject");
+String type = request.getParameter("type");
+int typeId = Integer.parseInt(request.getParameter("typeId"));
+
+%>
+<script type="text/javascript">
+function validateCommentForm()
+{
+	
+	return false;
+}
+</script>
+<div>
+<h1 class="c_head">Add comment</h1>
+
+<form method="post" action="" onsubmit="return validateCommentForm();">
+<table>
+	<tr>
+    	<td colspan="2">
+        <label for="subject">Subject</label><br />
+        <input type="text" name="subject" size="84" value="<%=subject %>" id="subject" /></td>
+    </tr>
+	<tr>
+    	<td colspan="2">
+        <br />
+        <label for="comment">Comment</label><br />
+        <textarea rows="5" cols="40" name="comment" id="comment"></textarea>
+        </td>
+    </tr>    
+	<tr>
+    <td colspan="2">
+    <label for="security_code">Please enter the security code</label>
+    <p><img src="${pageContext.request.contextPath}/getCaptcha.do?type=num" alt="captcha" /></p>
+    <input type="text" name="security_code" id="security_code" value="" />    
+    </td>
+    </tr>
+	<tr>    	
+        <td colspan="2">
+	        <input type="hidden" name="type" value="" />
+	        <input type="hidden" name="type_id" value="<%=typeId %>" />
+	        <input type="submit" name="submit" id="submit" value="Post"  />
+        </td>
+    </tr>  	  
+</table>
+</form>
+
+</div>
--- trunk/java-src/src/com/ipablive/services/CaptchaService.java
+++ trunk/java-src/src/com/ipablive/services/CaptchaService.java
@@ -5,23 +5,20 @@ package com.ipablive.services;
  */
 
     import java.awt.Color;
-    import java.awt.Font;
-    import java.awt.GradientPaint;
-    import java.awt.Graphics2D;
-    import java.awt.RenderingHints;
-    import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.OutputStream;
-
-import java.util.Hashtable;
-    import java.util.Random;
-import javax.imageio.ImageIO;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+	import java.awt.Font;
+	import java.awt.GradientPaint;
+	import java.awt.Graphics2D;
+	import java.awt.image.BufferedImage;
+	import java.io.IOException;
+	import java.io.OutputStream;
+	import java.util.Random;
+	import javax.imageio.ImageIO;
+	import javax.servlet.ServletConfig;
+	import javax.servlet.ServletException;
+	import javax.servlet.http.HttpServlet;
+	import javax.servlet.http.HttpServletRequest;
+	import javax.servlet.http.HttpServletResponse;
+	import javax.servlet.http.HttpSession;
 
 
 
@@ -31,17 +28,13 @@ import javax.servlet.http.HttpSession;
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-
-	private int height=0;
-	  private int width=0;
+	private static final String CAPCHA_TYPE_NUMBER = "num";
 	    
 	  public static String CAPTCHA_KEY = "";
 
 	  public void init(ServletConfig config) throws ServletException {
 	    super.init(config);
-	    height=30;
-	    width=120;
+	    
 	  }
 
 	 
@@ -53,17 +46,40 @@ import javax.servlet.http.HttpSession;
 	    response.setHeader("Pragma", "no-cache");
 	    response.setDateHeader("Max-Age", 0);
 	    
+	    String token = "";
+	    String ch = "";
+	    int height=0;
+	    int width=0;
+	    
+	    String type = req.getParameter("type");
+	    if(type==null)
+	    {
+	    	//alpha numeric
+	    	Random r = new Random();
+		    token = Long.toString(Math.abs(r.nextLong()), 36);
+		    ch = token.substring(0,6);
+		    height=32;
+		    width=122;
+	    }else if(type.equalsIgnoreCase(CAPCHA_TYPE_NUMBER))
+	    {
+	    	//creates a random number between 1000 to 9999
+	    	int x = (int)(Math.random() * 9999)+1000;
+	    	token = ""+x;
+	    	ch = token.substring(0,4);
+	    	height=32;
+		    width=78;
+	    }
+	    
+	    
 	    BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); 
 	    Graphics2D graphics2D = image.createGraphics();
-	    Random r = new Random();
-	    String token = Long.toString(Math.abs(r.nextLong()), 36);
-	    String ch = token.substring(0,6);
+	    
 	    Color c = new Color(0.6662f, 0.4569f, 0.3232f);
 	    GradientPaint gp = new GradientPaint(30, 30, c, 15, 25, Color.white, true);
 	    graphics2D.setPaint(gp);
 	    Font font=new Font("Verdana", Font.CENTER_BASELINE , 26);
 	    graphics2D.setFont(font);
-	    graphics2D.drawString(ch,2,20);
+	    graphics2D.drawString(ch,3,25);
 	    graphics2D.dispose();
 	    
 	    HttpSession session = req.getSession();
